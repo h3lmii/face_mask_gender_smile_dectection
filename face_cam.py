31c64f13dbb766c329365eb7dff421db592ced55
Helmi Massoussi <103006899+h3lmii@users.noreply.github.com>
2022-09-27 12:52:09 +0100

    Delete face_cam.py

--- face_cam.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import cv2
-def  main():
-        image_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
-
-
-if __name__ == '__main__':
-    main()
